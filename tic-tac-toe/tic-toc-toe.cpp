d9179fc6669807dc57f6001f00ea9b27c7ccf9e4
vermax2016 <vermax2016@gmail.com>
2023-12-17 12:08:35 +0300

    Функция main

--- tic-tac-toe/tic-toc-toe.cpp
+++ tic-tac-toe/tic-toc-toe.cpp
@@ -25,3 +25,31 @@ int humanMove(const vector<char>& board, char human);
 int computerMove(const vector<char>& board, char computer);
 void annonceWinner(char winner, char computer, char human);
 
+int main() 
+{
+	int move;
+	const int NUM_SQUARES = 9;
+	vector<char> board(NUM_SQUARES, EMPTY);
+	instructions();
+	char human = humanPiece();
+	char computer = opponent(human);
+	char turn = X;
+	displayBoard(board);
+	while (winner(board) == NO_ONE) 
+	{
+		if (turn == human)
+		{
+			move = humanMove(board, human);
+			board[move] = human;
+		}
+		else
+		{
+			move = computerMove(board, computer);
+			board[move] = computer;
+		}
+		displayBoard(board);
+		turn = opponent(turn);
+	}
+	annonceWinner(winner(board), computer, human);
+}
+
